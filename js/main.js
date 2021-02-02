99905d06185ca2fabf54d44514d3e7cf8da05c04
manhweird20112000 <dinhvanmanh20112000@gmail.com>
2021-02-02 18:32:29 +0700

    Fixed: Project

---
+++ js/main.js
@@ -0,0 +1,57 @@
+const song = [{
+        id: 1,
+        name: "Chương 2 Của Tương Lai",
+        image: "./images/chuong-2-cua-tuong-lai.jpeg",
+        artist: "Wean, MCK, Tenkitsune",
+        country: "Viet Nam"
+    },
+    {
+        id: 2,
+        name: "Ai Biet",
+        image: "./images/ai-biet.jpeg",
+        artist: "Wean",
+        country: "Viet Nam"
+    },
+    {
+        id: 3,
+        name: "Harder",
+        image: "./images/harder.jpeg",
+        artist: "Wxrdie, Wokeupat4am",
+        country: "Viet Nam"
+    },
+    {
+        id: 4,
+        name: "Cried",
+        image: "./images/cried.jpeg",
+        artist: "Vsoul",
+        country: "Viet Nam"
+    },
+    {
+        id: 5,
+        name: "Look At Me!",
+        image: "./images/look-at-me.jpeg",
+        artist: "XXXTENTACION",
+        country: "US/UK"
+    },
+    {
+        id: 6,
+        name: "Luffy / 19",
+        image: "./images/luffy-19.jpeg",
+        artist: "HS Robe, Chillinh",
+        country: "Viet Nam"
+    },
+    {
+        id: 7,
+        name: "Rap Cho Anh Em",
+        image: "./images/rap-cho-ae.jpeg",
+        artist: "OCEAN MOB, Willistic, Xolitxo, Gill, Obito",
+        country: "Viet Nam"
+    },
+    {
+        id: 8,
+        name: "Chơi Đồ",
+        image: "./images/choi-do.jpeg",
+        artist: "MCK, Wxrdie, Sony Tran",
+        country: "Viet Nam"
+    },
+]
